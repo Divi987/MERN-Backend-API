23c08ef5e54b3d38684345238c61d3ab26455973
Div <divyesh@Ubuntu.myguest.virtualbox.org>
2024-03-26 18:58:14 +0300

    Add Login routes with JWT, cookies and bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,60 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.route("/new").post((req, res) => {
   const userName = req.body.userName;
   const password = req.body.password;
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
 
-  const newUser = new User({ userName, password });
+  const newUser = new User({ ...req.body, password: hash });
 
   newUser
     .save()
-    .then(() => res.json("User Added"))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then(() => {
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT);
+      const { pass, ...othersData } = newUser._doc;
+      res
+        .cookie("access-token", token, {
+          httpOnly: true,
+        })
+        .status(201)
+        .json(othersData);
+    })
+    .catch((err) => res.status(400).json("Err: " + err));
 });
 
-router.route("/login").post((req, res) => {
-  const userName = req.body.userName;
-  const password = req.body.password;
+const login = async (req, res, next) => {
+  try {
+    const userName = req.body.userName;
 
-  const user = User.findOne({ userName, password });
+    const user = await User.findOne({ userName: userName });
 
-  console.log(user);
-  return user;
-    //.then(() => res.status(200).json("Logged IN"))
-    //.catch((e) => res.status(400).json("Error: " + e));
-});
+    console.log(user.password);
+
+    if (!user) return res.status(404).json("User Not Found");
+
+    const decodePass = await bcrypt.compare(req.body.password, user.password);
+
+    if (!decodePass) return res.status(400).json("Wrong Password");
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+
+    const { pass, ...userData } = user._doc;
+
+    res
+      .cookie("access-token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(userData);
+  } catch (err) {
+    next(err);
+  }
+};
+
+router.route("/login").post(login);
 
 module.exports = router;
